2cdec2b441b6d3de1069c7a11172046a644fb519
Sina Burow <sina.burow@sinnerschrader.com>
2020-03-26 16:40:16 +0100

    add todo

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 import styles from './todo.module.css';
+
+const Todo = ({todo}) => {
+    return (
+        <p className={`${styles.text} ${todo.completed && styles.completed}`}>
+            {todo.text}
+        </p>
+    )
+}
+
+export default Todo;
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,46 @@ import Todo from '../components/todo';
 
 export default () => {
     const [status, setStatus] = useState('loading');
-
     const [todos, setTodos] = useState(null);
+
+    useEffect(() => {
+        let canceled = false;
+
+        if (status !== 'loading') return;
+
+        axios('/api/get-all-todos').then(result => {
+            if (canceled === true) return;
+
+            if (result.status !== 200) {
+                console.error("error loading todos");
+                console.error(result);
+                return;
+            }
+
+            setTodos(result.data.todos);
+            setStatus('loading');
+        });
+
+        return () => {canceled = true;}
+    }, [status])
+
+    const reloadTodos = () => setStatus('loading');
 
     return (
         <main>
             <h1 className={styles.heading}>JAM stack todos</h1>
-            {todos ? (<p>t odos</p>) : (<p classsName={styles.loading}>loading ...</p>)}
+            <Form reloadTodos={reloadTodos} />
+            {todos ? (
+                <ul className={styles.todos}>
+                    {todos.map(todo => {
+                        return <li key={todo._id} className={styles.todo}>
+                            <Todo todo={todo} />
+                        </li>
+                    })}
+                </ul>
+            ) : (
+                <p className={styles.loading}>loading ...</p>
+            )}
         </main>
     )
 }
